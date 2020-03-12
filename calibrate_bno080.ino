ebb8fc074826eb19c507f52ad87af3568a09b158
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-03-12 10:33:31 -0700

    Ensure that calibrations are saved; remove reset

--- calibrate_bno080.ino
+++ calibrate_bno080.ino
@@ -13,8 +13,6 @@
 
 BNO080 _bno;
 
-
-
 void setup() {
   Serial.begin(9600);
   Serial.println();
@@ -22,10 +20,6 @@ void setup() {
   Wire.begin();
   // for i2c comms with bno080 we want 400 kHz clock
   Wire.setClock(400000);
-
-  digitalWrite(PIN_RESET, HIGH);
-  delay(200);
-  pinMode(PIN_RESET, OUTPUT);
 
   while (!_bno.begin(0x4a)) {
     Serial.println(F("No bno080..."));
@@ -43,7 +37,47 @@ void print_usage() {
   Serial.println(F(" 'l' for accel calibration "));
   Serial.println(F(" 'g' for gyro calibration "));
   Serial.println(F(" 'p' for planar accel calibration "));
+  Serial.println(F(" 'c' for check calibrations "));
   Serial.println(F(" 'a' for calibrate all "));
+}
+
+void check_calibration() {
+  Serial.println(F("Polling calibrations..."));
+
+  _bno.enableGyro(50);
+  _bno.enableAccelerometer(50);
+  _bno.enableMagnetometer(50);
+  _bno.enableRotationVector(250);
+
+  delay(1000);
+  for (int i = 0; i < 10; i++) {
+    if (_bno.dataAvailable()) {
+      uint8_t accel_status = _bno.getAccelAccuracy();
+      uint8_t gyro_status = _bno.getGyroAccuracy();
+      uint8_t mag_status = _bno.getMagAccuracy();
+      uint8_t quat_status = _bno.getQuatAccuracy();
+
+      Serial.print(F("Quat, Accel, Gyro, Mag  status: "));
+      Serial.print(quat_status);
+      Serial.print(F(", "));
+      Serial.print(accel_status);
+      Serial.print(F(", "));
+      Serial.print(gyro_status);
+      Serial.print(F(", "));
+      Serial.print(mag_status);
+      Serial.println();
+
+      float heading_accuracy = _bno.getQuatRadianAccuracy();
+      Serial.print(F("heading accuracy (radians): "));
+      Serial.println(heading_accuracy, 6);
+
+      delay(500);
+    }
+    else {
+      Serial.println(F("waiting..."));
+      delay(1000);
+    }
+  }
 }
 
 bool save_calibration() {
@@ -54,7 +88,7 @@ bool save_calibration() {
   _bno.requestCalibrationStatus();
   while (true) {
     if (_bno.dataAvailable()) {
-      if (_bno.calibrationComplete()) {
+      if (_bno.calibrationSaved()) {
         Serial.println(F("Calibration saved!"));
         _bno.endCalibration();
         return true;
@@ -71,7 +105,7 @@ bool calibrate_gyro() {
   Serial.println(F("Leave sensor still on flat surface, Z up"));
   _bno.calibrateGyro();
   _bno.enableGyro(50);
-  _bno.enableGameRotationVector(100);
+  _bno.enableRotationVector(250);
 
   //collect data while user leaves sensor still
   delay(1000);
@@ -97,7 +131,7 @@ bool calibrate_planar_accel() {
   Serial.println(F("Rotate sensor around Z axis"));
   _bno.calibratePlanarAccelerometer();
   _bno.enableAccelerometer(50);
-  _bno.enableGameRotationVector(100);
+  _bno.enableRotationVector(250);
 
   //collect data while user moves the sensor
   delay(1000);
@@ -126,7 +160,7 @@ bool calibrate_accel() {
   Serial.println(F("Calibrating 3D accel..."));
   _bno.calibrateAccelerometer();
   _bno.enableAccelerometer(50);
-  _bno.enableGameRotationVector(100);
+  _bno.enableRotationVector(250);
 
   Serial.println(F("Hold level with Z up "));
   Serial.println(F("Hold level with Z down "));
@@ -160,7 +194,7 @@ bool calibrate_mag() {
   Serial.println(F("Calibrating mag..."));
   _bno.calibrateMagnetometer();
   _bno.enableMagnetometer(50);
-  _bno.enableGameRotationVector(100);
+  _bno.enableRotationVector(250);
 
   Serial.println(F("Rotate around X"));
   delay(3000);
@@ -199,36 +233,33 @@ bool calibrate_all() {
 void loop() {
 
   if (Serial.available()) {
-    int cmd = Serial.read();
-    bool cal_performed = false;
+    String cmd_str = Serial.readStringUntil(10); //LF sent by serial monitor
+    int cmd = cmd_str[0];
     switch (cmd) {
       case 'm':
-        cal_performed = calibrate_mag();
+        calibrate_mag();
         break;
       case 'l':
-        cal_performed = calibrate_accel();
+        calibrate_accel();
         break;
       case 'p':
-        cal_performed = calibrate_planar_accel();
+        calibrate_planar_accel();
         break;
       case 'g':
-        cal_performed = calibrate_gyro();
+        calibrate_gyro();
         break;
       case 'a':
-        cal_performed = calibrate_all();
+        calibrate_all();
         break;
-      case -1:
-        //no command available
+      case 'c':
+        check_calibration();
         break;
       default:
-        Serial.println(F("huh?!"));
+        //Serial.print(F("huh?! "));
+        //Serial.println(cmd);
+        delay(1000);
         break;
     }
-
-    if (cal_performed) {
-      print_usage();
-    }
-
   }
 
 }
